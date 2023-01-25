fa09cadda774c4942fed72a41b60024ac79cfa96
Rafiere <rafapgbr@gmail.com>
2023-01-25 17:19:55 -0300

    feat: adding componentlogin component.

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -3,12 +3,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Dashboard, Login } from "../pages";
+import { LoginComponentizado } from "../pages/login-componentizado/LoginComponentizado";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />}></Route>
+        <Route
+          path="/component-login"
+          element={<LoginComponentizado />}
+        ></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="*" element={<h1>Página não encontrada!</h1>}></Route>
       </Routes>
