e19250c8ec7ce33e2a14f034081e9780cc386f9f
Rafiere <rafapgbr@gmail.com>
2023-01-25 21:10:30 -0300

    feat: creating "UsuarioLogadoContext" with contextApi.

---
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -0,0 +1,31 @@
+/* Cada arquivo na pasta "contexts" representará um único contexto. Podemos ter vários contextos dentro de uma aplicação React. */
+
+import React, { createContext } from "react";
+
+/* Essa é a interface que REPRESENTA OS DADOS COMPARTILHADOS DO CONTEXTO. */
+interface UsuarioLogadoContextData {
+  nomeDoUsuario: string;
+}
+
+/* Essa props foi criada apenas para passarmos o "children". O "children" não fará parte dos dados compartilhados pelo contexto, apenas representará os componentes que forem passados dentro desse provider. */
+interface UsuarioLogadoContextProps {
+  children: React.ReactNode;
+}
+
+/* Abaixo, vamos criar um contexto. Os dados abaixo poderão ser compartilhados entre diversas páginas. */
+
+/* O "as" representa que esse objeto vazio será do tipo da interface. */
+const UsuarioLogadoContext = createContext({} as UsuarioLogadoContextData);
+
+/* O componente abaixo será o PROVEDOR de um contexto. */
+
+/* Esse contexto compartilhará as informações que ele armazena com todo mundo que ele tiver como filhos. */
+const UsuarioLogadoProvider = ({ children }: UsuarioLogadoContextProps) => {
+  return (
+    <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: "Lucas" }}>
+      {children}
+    </UsuarioLogadoContext.Provider>
+  );
+};
+
+export default UsuarioLogadoProvider;
